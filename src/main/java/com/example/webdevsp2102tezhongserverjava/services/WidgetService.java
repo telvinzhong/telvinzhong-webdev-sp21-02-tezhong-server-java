7b3436caf0140edd0e6512c4d93a56a58dfb833d
Telvin <telvinzhong@gmail.com>
2021-03-21 13:40:08 -0700

    updated controller and service

--- src/main/java/com/example/webdevsp2102tezhongserverjava/services/WidgetService.java
+++ src/main/java/com/example/webdevsp2102tezhongserverjava/services/WidgetService.java
@@ -11,10 +11,10 @@ public class WidgetService {
     private List<Widget> widgets = new ArrayList<>();
     {
         Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
-        Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "LALALA");
+        Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Test");
         Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
-        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "LALALA");
-        Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "LALALA");
+        Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Test2");
+        Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Test3");
 
         widgets.add(w1);
         widgets.add(w2);
@@ -43,4 +43,27 @@ public class WidgetService {
         widgets.add(widget);
         return widget;
     }
+
+    public Integer deleteWidget(Long id) {
+        int index = -1;
+        for(int i = 0; i < widgets.size(); i++) {
+            if(widgets.get(i).getId().equals(id)) {
+                index = i;
+                widgets.remove(index);
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    public Integer updateWidget(Long id, Widget widget) {
+        for(int i = 0; i < widgets.size(); i++) {
+            if(widgets.get(i).getId().equals(id)) {
+                widgets.set(i, widget);
+                return 1;
+            }
+        }
+        return -1;
+    }
+
 }
